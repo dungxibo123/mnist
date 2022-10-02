1ae10587ade2c11c2f3073a2aa034547aaafb186
dungxibo123 <votiendung0805@gmail.com>
2022-10-02 22:30:31 +0700

    add torch.save for the checkpoint and the model

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def get_opt():
     args.add_argument("--use-gpu", action="store_true")
     args.add_argument("--test-size", default=0.2, type=float)
     args.add_argument("--workers", default=1, type=int)
+    args.add_argument("--label-smoothing", default=0.1, type=float)
 
 
     # Network setting
@@ -49,7 +50,8 @@ def get_opt():
 
     # Variables setting
     args.add_argument("--checkpoint", action='store_true')
-    args.add_argument("--checkpoint-path", type=str)
+    args.add_argument("--checkpoint-path", type=str, default="./checkpoints")
+    args.add_argument("--model-path", type=str, default="./model")
     args.add_argument("--data", choices=["MNIST","FMNIST"], required=True)
     args.add_argument("--data-path", type=str, required=True)
 
@@ -58,14 +60,40 @@ def get_opt():
     opt = args.parse_args()
     return opt
 
-def criterion(output, label):
-    return F.binary_cross_entropy_with_logits(output,label)
+def criterion():
+    return F.binary_cross_entropy_with_logits
+def evaluate(model, val_loader, opt):
+    model.eval()
+    correct = 0
+    total = 0 
+    running_loss = 0
+    count = 0 
+    with torch.no_grad():
+        for batch_id , test_data in enumerate(val_loader,0):
+            count += 1
+            data, label = test_data
+            label = F.one_hot(label, num_classes=opt.num_classes)
+            if opt.use_gpu:
+                data = data.to("cuda")
+                label = label.to("cuda")
+            outputs = model(data)
+            _, correct_labels = torch.max(label, 1) 
+            _, predicted = torch.max(outputs.data, 1)
+            total += label.size(0)
+            correct += (predicted == correct_labels).sum().item()
+            running_loss += criterion()(
+                outputs.float(), label.float()).item()
+    #        print(f"--> Total {total}\n-->batch_id: {batch_id + 1}")
+    acc = round(correct/total * 1.0 , 5)
+    running_loss /= count 
+    return running_loss, acc
 def train_one_iter(model, optim, train_load, val_loader, opt, epoch):
     losses = 0
     model.train()
     with tqdm(train_load,  unit="batch", position=0, leave=True) as tp:
         tp.set_description(f"Epoch {epoch}/{opt.epoch}")
         for (batch_x, batch_y) in tp:
+             
             optim.zero_grad()
             batch_y = torch.nn.functional.one_hot(batch_y, num_classes = opt.num_classes)
             if opt.use_gpu:
@@ -73,16 +101,21 @@ def train_one_iter(model, optim, train_load, val_loader, opt, epoch):
                 batch_y = batch_y.to("cuda")
             #print(batch_x.shape, batch_y.shape)
             outputs = model(batch_x)
-            loss = criterion(outputs.float(), batch_y.float())
+            _, correct_labels = torch.max(batch_y, 1)
+            _, predicted = torch.max(outputs.data, 1)
+            total = batch_y.size(0)
+            correct = (predicted == correct_labels).sum().item()
+            train_acc = round(correct / total, 5)
+            loss = criterion()(outputs.float(), batch_y.float())
             loss_item = loss.item()
             losses += loss_item
             loss.backward()
             optim.step()
-            tp.set_postfix(loss=loss_item)
+            tp.set_postfix(loss=loss_item, train_acc=train_acc)
         val_loss, val_acc = evaluate(model, val_loader, opt)
         print(f"Epoch {epoch}/{opt.epoch} is finished with validation accuracy is {val_acc}")
 
-    return model, optim, losses
+    return model, optim, losses, val_loss, val_acc
 
 def main():
     opt = get_opt()
@@ -108,37 +141,37 @@ def main():
     #for i in tqdm.tqdm(range(opt.epoch)):
 
     #with tqdm(range(1, opt.epoch + 1), position=0, leave=True) as tp:
+    best_model = None
+    best_val_acc = 0
+    best_epoch = -1
+    losses = []
+    val_accuracies = []
+    val_losses = []
     for i in range(1, opt.epoch+1): 
 
         
-        model, optim, epoch_loss = train_one_iter(model, optim, train_loader, val_loader, opt, epoch = i)
+        model, optim, epoch_loss, val_loss, val_acc = train_one_iter(model, optim, train_loader, val_loader, opt, epoch = i)
+        losses.append(epoch_loss)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_acc)
+        if i % 5 == 0 and opt.checkpoint:
+            torch.save({
+                'opt': opt,
+                'epoch': i,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'losses': losses,
+            }, opt.checkpoint_path + "/model.pt")
+        if val_acc > best_val_acc:
+            best_model = model
+            best_val_acc = val_acc
+            best_epoch = i
         #tp.set_postfix(loss=epoch_loss, val_loss=val_loss, val_acc=val_acc)
-        
 
-def evaluate(model, val_loader, opt):
-    model.eval()
-    correct = 0
-    total = 0 
-    running_loss = 0
-    count = 0 
-    with torch.no_grad():
-        for batch_id , test_data in enumerate(val_loader,0):
-            count += 1
-            data, label = test_data
-            label = F.one_hot(label, num_classes=opt.num_classes)
-            if opt.use_gpu:
-                data = data.to("cuda")
-                label = label.to("cuda")
-            outputs = model(data)
-            _, correct_labels = torch.max(label, 1) 
-            _, predicted = torch.max(outputs.data, 1)
-            total += label.size(0)
-            correct += (predicted == correct_labels).sum().item()
-            running_loss += torch.nn.functional.binary_cross_entropy_with_logits(
-                outputs.float(), label.float()).item()
-    #        print(f"--> Total {total}\n-->batch_id: {batch_id + 1}")
-    acc = round(correct/total * 1.0 , 5)
-    running_loss /= count 
-    return running_loss, acc 
+    torch.save({
+        'opt': opt,
+        'model_state_dict':model.state_dict(),
+    }, opt.model_path + "/final_model.pt")    
+
 if __name__=="__main__":
     main()
